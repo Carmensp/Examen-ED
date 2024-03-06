5ecc9db682fe7a0b9bfc025989e7e54348d4604f
Carmen <carmen.8.2.0.3.sp@gmail.com>
2024-03-06 18:45:32 +0100

    Segunda modificación rama A

--- Principal.java
+++ Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola a tod@s");
-		System.out.println("Primera modificacion Rama A");
+		System.out.println("Segunda modificacion Rama A");
 
 	}
 
